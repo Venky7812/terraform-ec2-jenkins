e6f81847f051037da7d568b0ce2d90fcb6d979f2
Venky7812 <guggullabalaiahgari766@gmail.com>
2025-03-28 18:01:11 +0530

    8th commit

--- s3.tf
+++ s3.tf
@@ -4,9 +4,20 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "jenkins_s3_bucket" {
-  bucket = "my-unique-jenkins-s3-bucket"  # Change to a globally unique bucket name
+  bucket = "my-unique-jenkins-s3-bucket" 
 
   tags = {
     Name        = "jenkins S3 Bucket"
   }
+
+}
+resource "aws_s3_bucket" "jenkins_s3_bucket" {
+  bucket = "my-unique-jenkins-s3-bucket"
+}
+
+resource "aws_s3_bucket" "upload_file" {
+  bucket = aws_s3_bucket.my-unique-jenkins-s3-bucket.id
+  key    = "myfile.txt"  
+  source = "myfile.txt"  
+  acl    = "private"
 }
