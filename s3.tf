1d0725c2841cb73af079d42ad3a1687144199391
Venky7812 <guggullabalaiahgari766@gmail.com>
2025-03-28 17:55:11 +0530

    7th commit

--- s3.tf
+++ s3.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  # Change the region as needed
+  region = "us-east-1"
+  alias  = "s3"
 }
 
 resource "aws_s3_bucket" "jenkins_s3_bucket" {
   bucket = "my-unique-jenkins-s3-bucket"  # Change to a globally unique bucket name
 
   tags = {
-    Name        = "Jenkins S3 Bucket"
+    Name        = "jenkins S3 Bucket"
   }
 }
