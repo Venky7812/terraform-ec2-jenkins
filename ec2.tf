2288c85dc203a71e36cd6efce9e1c8aa452f7318
Venky7812 <guggullabalaiahgari766@gmail.com>
2025-03-28 17:35:46 +0530

    5th commit

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web"{
     ami           = "ami-084568db4383264d4"
     instance_type = "t2.micro"
     key_name      = "jenkins"
+    subnet_id     = "subnet-064a27597a4e295e9"
     tags = {
         Name = "My EC2 Instance"
     }
